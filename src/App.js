68cd6383aad9a2a6b93728b1674f174c6b5b960b
NalsonMark <nalsonmark349@gmail.com>
2023-09-26 16:26:25 +0800

    Ded

--- src/App.js
+++ src/App.js
@@ -5,19 +5,22 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Welcome to My React App</h1>
+        <p>This is a perfect static page template.</p>
       </header>
+      <main className="App-content">
+        <section>
+          <h2>Section 1</h2>
+          <p>This is section 1 content.</p>
+        </section>
+        <section>
+          <h2>Section 2</h2>
+          <p>This is section 2 content.</p>
+        </section>
+      </main>
+      <footer className="App-footer">
+        <p>&copy; 2023 Your Name</p>
+      </footer>
     </div>
   );
 }
